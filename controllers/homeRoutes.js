2fa6eddcdbe2598b55d7b25a236671a3df4257cf
D3C0D1NG <joseph.s.foster@icloud.com>
2023-10-03 19:24:15 -0500

    testing server

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,16 +4,16 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all posts and JOIN with user data
-    const postData = await Post.findAll({
-      include: [User],
-    });
+    // // Get all posts and JOIN with user data
+    // const postData = await Post.findAll({
+    //   include: [User],
+    // });
 
-    // Serialize data so the template can read it
-    const posts = postData.map((post) => post.get({ plain: true }));
+    // // Serialize data so the template can read it
+    // const posts = postData.map((post) => post.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
-    res.render('homepage', { posts, loggedIn: req.session.logged_in });
+    // // Pass serialized data and session flag into template
+    res.render('homepage', { loggedIn: req.session.logged_in });
   } catch (err) {
     res.status(500).json(err);
   }

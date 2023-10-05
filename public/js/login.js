d57f01806d9bca719e781d84dcc2a8ae8d27d015
Molly Starnes <mstrnes2@gmail.com>
2023-10-05 18:14:07 -0500

    fixed the sign up button

--- public/js/login.js
+++ public/js/login.js
@@ -1,49 +1,46 @@
 const loginFormHandler = async (event) => {
-    event.preventDefault();
-    console.log("test");
-    const email = document.querySelector('#email-login').value.trim();
-    const password_hash = document.querySelector('#password-login').value.trim();
-  
-    if (email && password_hash) {
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password_hash }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/session');
-      } else {
-        alert('Failed to log in.');
-      }
-    }
-  };
-  
-  const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password_hash = document.querySelector('#password-login').value.trim();
-    console.log(email, password_hash);
-    if (email && password_hash) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ email, password_hash }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to sign up.');
-      }
-    }
-  };
+  event.preventDefault();
+  console.log("test");
+  const email = document.querySelector("#email-login").value.trim();
+  const password_hash = document.querySelector("#password-login").value.trim();
 
-  document
-    .querySelector('#login')
-    .addEventListener('click', loginFormHandler);
-  
-  document
-    .querySelector('#signup')
-    .addEventListener('click', signupFormHandler);
+  if (email && password_hash) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password_hash }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/session");
+    } else {
+      alert("Failed to log in.");
+    }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector("#email-login").value.trim();
+  const password_hash = document.querySelector("#password-login").value.trim();
+  console.log(email, password_hash);
+  if (email && password_hash) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ email: email, password_hash: password_hash }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/session");
+    } else {
+      console.log(response);
+      console.log("Failed to sign up.");
+    }
+  }
+};
+
+document.querySelector("#login").addEventListener("click", loginFormHandler);
+
+document.querySelector("#signup").addEventListener("click", signupFormHandler);

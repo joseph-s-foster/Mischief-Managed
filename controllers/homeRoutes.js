0c77019d3fa310e98ab78ce69e53102dcc9cd300
D3C0D1NG <joseph.s.foster@icloud.com>
2023-10-05 20:38:25 -0500

    resolved route handling

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,4 +10,37 @@ router.get('/', async (req, res) => {
   }
 });
 
-module.exports = router
+router.get('/session', async (req, res) => {
+  try {
+    // Fetch data from the models
+    const users = await User.findAll();
+    const books = await Book.findAll();
+    const triviaItems = await Trivia.findAll();
+    const userTrivia = await UserTrivia.findAll();
+
+    // Pass the fetched data to the 'session' view
+    res.render('session', {
+      layout: 'main',
+      loggedIn: req.session.logged_in,
+      users,
+      books,
+      triviaItems,
+      userTrivia
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// router.post('/session', async (req, res) => {
+//   try {
+//     // Handle form submission logic here
+//     // For example, you can update user session based on form data
+//     req.session.logged_in = true;
+//     res.redirect('/session'); // Redirect to the session page after form submission
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const session = require('./api/session');
 router.use('/api', apiRoutes);
 router.use("/", homeRoutes);
-router.use("/session", session);
 module.exports = router;

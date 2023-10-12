869c412a08d70b7658af53642e41b2c30f3b88d7
Molly Starnes <mstrnes2@gmail.com>
2023-10-12 18:55:20 -0500

    cleaned up the route files

--- controllers/api/bookRoutes.js
+++ controllers/api/bookRoutes.js
@@ -1,32 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { Book, Trivia, UserTrivia } = require("../../models");
-const sessionRoutes = require("./session");
+const { Trivia, UserTrivia } = require("../../models");
 const withAuth = require('../../utils/auth')
-// Include sessionRoutes
-router.use("/session", sessionRoutes);
-
-// Get a single book by ID
-// router.get("/display/:id", async (req, res) => {
-//   try {
-//     const bookData = await Book.findByPk(req.params.id);
-
-//     if (!bookData) {
-//       res.status(404).json({ message: "No book found with that id!" });
-//       return;
-//     }
-
-//     console.log("Image URL:", bookData.image); // Log the image URL
-
-//     res.render("singleBook", {
-//       layout: "main",
-//       title: bookData.title, // Pass the book title
-//       image: bookData.image, // Pass the image URL
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 router.get("/trivia/:bookid/:milestone", withAuth, async (req, res) => {
   try {

1feeaa0eaeea8ab66aaf18d88c13483ab4e20d45
D3C0D1NG <joseph.s.foster@icloud.com>
2023-10-11 20:00:32 -0500

    refactored existing routes and hbs, added js

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ class User extends Model {
 User.init(
   {
     user_id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.BIGINT,
       autoIncrement: true,
       primaryKey: true,
     },

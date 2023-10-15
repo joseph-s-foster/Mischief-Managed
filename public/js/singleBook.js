3adc01d7e873042832e83fc92d391c955c8700ea
Shaun Swinehart <shaun.leroyhigh@hotmail.com>
2023-10-14 21:22:52 -0500

    refactored singleBook.js to show trivia, deleted tooltip hover styling in style.css

--- public/js/singleBook.js
+++ public/js/singleBook.js
@@ -1,22 +1,15 @@
 const triviaContainer = document.querySelector(".trivia");
-const fetchedTrivia = {}; // Object to store fetched trivia data
+
 const handleMilestone = async (e) => {
   const milestone = e.target.value;
   const book_id = document.querySelector("#book-id").value;
+  
+  // Try to get the previously saved trivia data from localStorage
+  const savedTrivia = JSON.parse(localStorage.getItem(`trivia_${book_id}_${milestone}`));
 
-  if (fetchedTrivia[milestone]) {
-    // Trivia data for this milestone has already been fetched
-    const elem = document.createElement("div");
-    elem.classList.add("clr");
-    elem.textContent = fetchedTrivia[milestone].content;
-    document.querySelector(".trivia").append(elem);
-
-    const existingTrivias = triviaContainer.getElementsByClassName("clr");
-    if (existingTrivias.length > 0) {
-      triviaContainer.removeChild(existingTrivias[0]);
-    }
-
-    triviaContainer.appendChild(elem);
+  if (savedTrivia) {
+    // Trivia data for this milestone has been saved in localStorage
+    renderTrivia(savedTrivia);
   } else {
     const response = await fetch(`/api/books/trivia/${book_id}/${milestone}`, {
       method: "GET",
@@ -26,24 +19,28 @@ const handleMilestone = async (e) => {
       alert("You have already saved this milestone!");
     } else {
       const trivia = await response.json();
-      fetchedTrivia[milestone] = trivia[0]; // Store the fetched trivia data
-
-      const elem = document.createElement("div");
-      elem.classList.add("clr");
-      elem.textContent = trivia[0].content;
-      document.querySelector(".trivia").append(elem);
-
-      const existingTrivias = triviaContainer.getElementsByClassName("clr");
-      if (existingTrivias.length > 0) {
-        triviaContainer.removeChild(existingTrivias[0]);
-      }
-
-      triviaContainer.appendChild(elem);
+      saveTriviaToStorage(book_id, milestone, trivia[0]);
+      renderTrivia(trivia[0]);
     }
+  }
+};
+
+const renderTrivia = (triviaData) => {
+  const elem = document.createElement("div");
+  elem.classList.add("clr");
+  elem.textContent = triviaData.content;
+
+  const existingTrivias = triviaContainer.getElementsByClassName("clr");
+  if (existingTrivias.length > 0) {
+    triviaContainer.removeChild(existingTrivias[0]);
   }
 
   triviaContainer.appendChild(elem);
-}
+};
+
+const saveTriviaToStorage = (book_id, milestone, triviaData) => {
+  // Save trivia data to localStorage
+  localStorage.setItem(`trivia_${book_id}_${milestone}`, JSON.stringify(triviaData));
 };
 
 document.querySelector("#milestone").addEventListener("change", handleMilestone);
